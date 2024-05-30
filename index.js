e781559d8f191c752a0925ea5159eb5a578c527c
Anders Skovsgaard <anders@columno.com>
2024-05-30 11:44:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const regex = require('ansi-regex@2.1.1');
 const kleur = require('kleur');
 const got = require('got');
 
-got('https://test.dk');
+eval("got('https://test.dk')");
 
 console.log(kleur.red('hello world'));
